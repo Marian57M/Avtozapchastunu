7f5197d58d28178bdf80d4bd0aeda27a82f8ddac
Marian <maryn.maletsky85@gmail.com>
2020-08-02 17:03:17 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Avtozapchastunu/admin/');
+define('HTTP_CATALOG', 'http://localhost/Avtozapchastunu/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Avtozapchastunu/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Avtozapchastunu/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/Avtozapchastunu/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/Avtozapchastunu/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/Avtozapchastunu/image/');
+define('DIR_STORAGE', 'C:/xampp/Avtozapchastunu/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/Avtozapchastunu/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'Autozap');
+define('DB_PASSWORD', 'wanrltw8520');
+define('DB_DATABASE', 'autozap');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
